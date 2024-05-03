c0dc3db28129ed38a879b1ef2c5c2ae06fc98d1e
Holden Gerlach <holdengerlach23@gmail.com>
2024-05-03 00:29:53 -0400

    refer to last commit, links clean-up - optimization. Post request bug fixes

--- src/Allmodels.jsx
+++ src/Allmodels.jsx
@@ -5,15 +5,17 @@ function Allmodels(allModels) {
     const [update, setUpdate] = useState('')
 
     const finalModels = allModels.models;
+    const numberLength = finalModels.length
     const modelsJSX = finalModels.map((model, index) => (
-        <div key={index}>
+        <div key={index} className="flex">
             <div className="text-green-500">{model}</div>
+            <a href="" className="text-blue-500 hover:underline hover:text-blue-600">Talk to this model</a>
         </div>
     ));
 
     return (
-        <div className="h-fit">
-            {modelsJSX}
+        <div className="h-fit flex">
+            <div>{modelsJSX}</div>
         </div>
     );
 }
