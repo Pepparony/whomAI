716c2b5fc8de879d2543623219faa8e6d618fb03
Holden Gerlach <holdengerlach23@gmail.com>
2024-05-04 15:16:24 -0400

    fixes, refer to the last commit for up to date changes.

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,9 +1,20 @@
-
+import { CookiesProvider, useCookies  } from 'react-cookie'
+import Login from './Login.jsx'
+import Chatting from './Chatting.jsx'
 
 function Chat() {
 
-    return(
-        <div>Hello world</div>
+    const [cookies, setCookie] = useCookies(['user'])
+    
+    function dealWithLogin(check) {
+        setCookie('user', check.data.cookie, { path: '/' })
+    }
+
+    
+    return (    
+        <CookiesProvider>
+            <div>{cookies.user || cookies.user == 'undefined' ? <Chatting cookie={cookies.user}/> : <Login handleLogin={dealWithLogin}/>}</div>
+        </CookiesProvider >
     )
 }
 
---
+++ src/Chatting.jsx
@@ -0,0 +1,10 @@
+
+
+function Chatting() {
+
+    return(
+        <div>You are now chatting with the bot</div>
+    )
+}
+
+export default Chatting;
