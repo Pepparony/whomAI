9e05a106e1f61df97cf793e0d67db1513ab28a31
Holden Gerlach <holdengerlach23@gmail.com>
2024-05-06 14:32:43 -0400

    working?

--- backend/server.cjs
+++ backend/server.cjs
@@ -1,4 +1,6 @@
+
 // Import all required packages
+
 
 const express = require('express')
 const mongoose = require('mongoose')
@@ -10,15 +12,18 @@ const session = require('express-session')
 const bcrypt = require('bcrypt')
 const Model = require('../models/modelsSchema.cjs')
 
+
 // Setting up the session to store user cookies
 app.use(express.urlencoded({ extended: true }));
 const sessionConfig = {
-    secret: 'BigHomieBalling',
-    resave: false,
-    saveUninitialized: true,
+   secret: 'BigHomieBalling',
+   resave: false,
+   saveUninitialized: true,
 }
 app.use(session(sessionConfig))
 app.use(express.json());
+
+
 
 
 // Add needed packages into the app, cors for the request and JSON to communicate data
@@ -26,193 +31,206 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+
 // Get variables from the ENV file - Includes API key
 require('dotenv').config()
 
+
 //setting up the MongoDB database
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Database connected successfully");
-    })
-    .catch(err => {
-        console.error("Database connection error:", err);
-    });
+   .then(() => {
+       console.log("Database connected successfully");
+   })
+   .catch(err => {
+       console.error("Database connection error:", err);
+   });
 // The post request to access the API data
 app.post('/request', async (req, res) => {
-    try {
-        const anthropic = new Anthropic({
-            apiKey: process.env.API_KEY
-        });
-        const msg = await anthropic.messages.create({
-            model: 'claude-2.1',
-            max_tokens: 1024,
-            messages: [
-                { "role": "user", "content": `${req.body.messages}` }
-            ]
-        });
-        // Send the JSON
-        res.send(msg.content)
-    }
-    catch (err) {
-        console.log(err)
-    }
+   try {
+       const {identity, messages} = req.body
+       const model = await Model.findOne({
+           _id: identity
+       })
+       const anthropic = new Anthropic({
+           apiKey: process.env.API_KEY
+       });
+       const msg = await anthropic.messages.create({
+           model: 'claude-2.1',
+           max_tokens: 1024,
+           messages: [
+             {"role": "user", "content": `I want you to be my friend ${model.modelName}. Please speak to me like this:${model.modelDescription} using these key words consistently throughout your message:${model.frequentWords}. Here is the message: ${req.body.messages}`}
+           ]
+         });
+         // Send the JSON
+       res.send(msg.content)
+   }
+   catch (err) {
+       console.log(err)
+   }
 })
+
+
 
 
 app.post('/register', async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
-        const search = await User.findOne({ email: email })
-        if (search) {
-            return res.json({
-                error: 'email is already in use'
-            })
-        }
-        if (!password) {
-            return res.json({
-                error: 'password is required'
-            })
-        }
-        if (!email) {
-            return res.json({
-                error: 'email is required'
-            })
-        }
-        if (!username) {
-            return res.json({
-                error: 'username is required'
-            })
-        }
-        if (!search) {
-            const finalPassword = await bcrypt.hash(password, 12)
-            const user = await User.create({
-                email: email,
-                username: username,
-                userPassword: finalPassword,
-            })
-            await user.save()
-            return res.json({
-                message: 'whomAI account created successfully',
-                cookie: user._id
-            })
-        }
-        else {
-            return res.json({
-                error: 'email is already in use'
-            })
-        }
-    }
-    catch (err) {
-        console.log(err)
-    }
+   try {
+       const { email, username, password } = req.body;
+       const search = await User.findOne({ email: email })
+       if (search) {
+           return res.json({
+               error: 'email is already in use'
+           })
+       }
+       if (!password) {
+           return res.json({
+               error: 'password is required'
+           })
+       }
+       if (!email) {
+           return res.json({
+               error: 'email is required'
+           })
+       }
+       if (!username) {
+           return res.json({
+               error: 'username is required'
+           })
+       }
+       if (!search) {
+           const finalPassword = await bcrypt.hash(password, 12)
+           const user = await User.create({
+               email: email,
+               username: username,
+               userPassword: finalPassword,
+           })
+           await user.save()
+           return res.json({
+               message: 'whomAI account created successfully',
+               cookie: user._id
+           })
+       }
+       else {
+           return res.json({
+               error: 'email is already in use'
+           })
+       }
+   }
+   catch (err) {
+       console.log(err)
+   }
 })
+
 
 app.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body
-        const user = await User.findOne({ email: email })
-        if (!user) {
-            return res.json({
-                erorr: 'error'
-            })
-        }
-        const result = await bcrypt.compare(password, user.userPassword)
-        if (result) {
-            req.session.user_id = user._id
-            return res.json({
-                message: 'Login successful, welcome back',
-                cookie: user._id,
-                redirectLink: 'http://localhost5173'
-            })
-        }
-        else {
-            return res.json({
-                error: 'error'
-            })
-        }
-    }
-    catch (err) {
-        console.log(err)
-    }
+   try {
+       const { email, password } = req.body
+       const user = await User.findOne({ email: email })
+       if (!user) {
+           return res.json({
+               erorr: 'error'
+           })
+       }
+       const result = await bcrypt.compare(password, user.userPassword)
+       if (result) {
+           req.session.user_id = user._id
+           return res.json({
+               message: 'Login successful, welcome back',
+               cookie: user._id,
+               redirectLink: 'http://localhost5173'
+           })
+       }
+       else {
+           return res.json({
+               error: 'error'
+           })
+       }
+   }
+   catch (err) {
+       console.log(err)
+   }
 })
+
 
 app.post('/createmodel', async (req, res) => {
-    try {
-        const { name, frequentWords, description,sampleText, author } = req.body
-        if (!name) {
-            return res.json({
-                error: 'You must provide a name'
-            })
-        }
-        if (!description) {
-            return res.json({
-                error: 'You must provide a description'
-            })
-        }
-        else {
-            const model = await Model.create({
-                modelName: name,
-                frequentWords: frequentWords,
-                modelDescription: description,
-                sampleText: sampleText,
-                author: author,
-            })
-            await model.save()
-            return res.json({
-                message: 'model was created successfully'
-            })
-        }
-    }
-    catch (err) {
-        console.log(`error at createmodel post route: ${err}`)
-    }
+   try {
+       const { name, frequentWords, description,sampleText, author } = req.body
+       if (!name) {
+           return res.json({
+               error: 'You must provide a name'
+           })
+       }
+       if (!description) {
+           return res.json({
+               error: 'You must provide a description'
+           })
+       }
+       else {
+           const model = await Model.create({
+               modelName: name,
+               frequentWords: frequentWords,
+               modelDescription: description,
+               sampleText: sampleText,
+               author: author,
+           })
+           await model.save()
+           return res.json({
+               message: 'model was created successfully'
+           })
+       }
+   }
+   catch (err) {
+       console.log(`error at createmodel post route: ${err}`)
+   }
 })
+
 
 app.post('/mymodels', async (req,res) => {
-    try{
-        const {identity} = req.body
-        const model = await Model.find({
-            author: identity
-        })
-        if(!model) {
-            return res.json({
-                message: 'You dont have any models'
-            })
-        }
-        if(model) {
-            return res.json({
-                message: model
-            })
-        }
-    }
-    catch(err) {
-        console.log(`error on post request to /mymodels ${err}`)
-    }
+   try{
+       const {identity} = req.body
+       const model = await Model.find({
+           author: identity
+       })
+       if(!model) {
+           return res.json({
+               message: 'You dont have any models'
+           })
+       }
+       if(model) {
+           return res.json({
+               message: model
+           })
+       }
+   }
+   catch(err) {
+       console.log(`error on post request to /mymodels ${err}`)
+   }
 })
 
+
 app.post('/chat', async (req,res) => {
-    try{
-        const {identity} = req.body
-        const model = await Model.find({
-            _id: identity
-        })
-        if(!model) {
-            return res.json({
-                message: 'no model found'
-            })
-        }
-        if(model) {
-            return res.json({
-                message: model
-            })
-        }
-    }
-    catch(err) {
-        console.log(`error on post request to /mymodels ${err}`)
-    }
+   try{
+       const {identity} = req.body
+       const model = await Model.find({
+           _id: identity
+       })
+       if(!model) {
+           return res.json({
+               message: 'no model found'
+           })
+       }
+       if(model) {
+           return res.json({
+               message: model
+           })
+       }
+   }
+   catch(err) {
+       console.log(`error on post request to /mymodels ${err}`)
+   }
 })
+
 
 // The Localhost port that the server is being hosted on
 app.listen(3000, (req, res) => {
-    console.log('LISTENING ON PORT 3000')
+   console.log('LISTENING ON PORT 3000')
 })
--- src/Chatting.jsx
+++ src/Chatting.jsx
@@ -1,53 +1,70 @@
 import {useParams} from 'react-router-dom'
 import axios from 'axios'
 import Notfound from './Notfound.jsx'
+import {useState, useEffect} from 'react'
+
 
 function Chatting() {
-    const params = useParams()
-    const userID = params.userID
-    const name = params.name
-    // async function getData() {
-    //     const check = await axios.post('http://localhost:3000/chat', { identity: userID })
-    //     if(check.data.message === 'no model found') {
-    //         return(
-    //             <Notfound/>
-    //         )
-    //     }
-    //     if(check) {
-    //         return(
-    //             <>
-    //             <div>You are not chatting with this model:</div>
-    //             <div>{check.data.message}</div>
-    //             </>
-    //         )
-    //     }
-    // }
+   const params = useParams()
+   const userID = params.userID
+   const name = params.name
+   const [msg, setMsg] = useState('')
+   const [response, setResponse] = useState('')
 
-    async function sendMessage(){
-        const response = await axios.post('http://localhost:3000/request', {identity: userID})
 
-        if(!response) {
-            console.log('No response came back :(, refer to backend console for error information')
-        }
-    }
-    return (
-        <div className="flex flex-col h-screen w-screen bg-zinc-800">
-            <section className="flex flex-col place-items-center place-self-center w-4/5 h-[5%] md:h-[10%] bg-zinc-800">
-                <a href="/details" className="font-bold text-3xl tracking-wide text-gray-100">{name}</a>
-                <a className="text-blue-500 hover:underline hidden md:inline" href="/details">details about model</a>
-            </section>
-            <section className="h-[90%] flex flex-col place-items-center justify-center">
-                <img src="/src/assets/robotWithRemovedBackground.png" alt="" />
-                    <div className="text-center text-xl text-white">What up?</div>
-            </section>
-            <section className="h-[10%] bg-zinc-800 grid place-items-center w-full">
-                <div className="flex h-3/5 w-full justify-center space-x-3">
-                <input className="border border-zinc-700 bg-zinc-800 text-white rounded-xl py-1 px-2" type="text" placeholder="How are you?"/>
-                <button onClick={sendMessage} className="text-gray-100">≥≥≥</button>
-                </div>
-            </section>
-        </div>
-    )
+   const editMsg = (evt) => {
+       const message = evt.target.value
+       setMsg(message)
+   }
+  
+
+
+   async function sendMessage(){
+       try {
+       setMsg('')
+       const response = await axios.post('http://localhost:3000/request', {identity: userID, messages:msg})
+       if(response) {
+           setResponse(response.data[0].text)
+       }
+       if(!response) {
+           console.log('No response came back :(, refer to backend console for error information')
+       }
+       else {
+           console.log('request was never sent to the backend')
+       }
+   }
+   catch(err) {
+       console.log(`there was an error: ${err}`)
+   }
+   }
+   function messagesInfo() {
+       return (<section className="h-[90%] flex flex-col place-items-center justify-center">
+       <img src="/src/assets/robotWithRemovedBackground.png" alt="" />
+           <div className="text-center text-xl text-white">What up?</div>
+   </section>)
+   }
+   return (
+       <div className="flex flex-col h-screen w-screen bg-zinc-800">
+           <section className="flex flex-col place-items-center place-self-center w-4/5 h-[5%] md:h-[10%] bg-zinc-800">
+               <a href="/details" className="font-bold text-3xl tracking-wide text-gray-100">{name}</a>
+               <a className="text-blue-500 hover:underline hidden md:inline" href="/details">details about model</a>
+           </section>
+           <section className="h-[90%] flex flex-col place-items-center justify-center">
+       <img src="/src/assets/robotWithRemovedBackground.png" alt="" />
+           <div className="text-center text-xl text-white">What up?</div>
+           <div>
+               <p className="text-gray-100">{response}</p>
+           </div>
+   </section>
+           <section className="h-[10%] bg-zinc-800 grid place-items-center w-full">
+               <div className="flex h-3/5 w-full justify-center space-x-3">
+               <input onChange={editMsg} value={msg} className="border border-zinc-700 bg-zinc-800 text-white rounded-xl py-1 px-2" type="text" placeholder="How are you?"/>
+               <button onClick={sendMessage} className="text-gray-100">≥≥≥</button>
+               </div>
+           </section>
+       </div>
+   )
 }
+
 
 export default Chatting;
